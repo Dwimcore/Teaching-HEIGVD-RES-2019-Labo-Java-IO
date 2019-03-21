92255ff4ee982d914a50fa36ebdfb8e2bf4c3ee1
Dwimcore <fournier.robin@gmail.com>
2019-03-21 18:41:21 +0100

    Final Tests

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
@@ -1,5 +1,8 @@
 package ch.heigvd.res.labio.impl;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.logging.Logger;
 
 /**
@@ -20,7 +23,43 @@ public class Utils {
    * contain any line separator, then the first element is an empty string.
    */
   public static String[] getNextLine(String lines) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    //throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    String[] result = new String[2];
+    result[0] = new String("");
+    boolean containsLineSep = true;
+    List<String> t;
+    String separator ="";
+    System.out.print("Le separateur est : ");
+
+
+    if(lines.contains("\n\r")){
+      separator = "\n\r";
+      System.out.println("win");
+    } else if(lines.contains("\n")){
+      separator = "\n";
+      System.out.println("nux");
+    } else if(lines.contains("\r")){
+      separator = "\r";
+      System.out.println("mac");
+    } else {
+      System.out.println("aucun");
+      containsLineSep = false;
+    }
+
+    t = new ArrayList<>(Arrays.asList(lines.split(separator)));
+
+    if(containsLineSep) {
+      result[1] = new String("");
+      result[0] = (t.get(0)+separator);
+      t.remove(0);
+      for(String s : t){
+        result[1] = result[1].concat(s+separator);
+      }
+    }
+    else
+      result[1] = lines;
+
+    return result;
   }
 
 }
